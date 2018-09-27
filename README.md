ee6eb8ad9f9d88bd969c5aa935caf623d5d57419
Bryan Gruneberg <hello@bryangruneberg.com>
2018-09-27 17:39:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## SlackTail = SLAIL
+
+### Installation
+Simple install for now... clone this repo, run composer install. You'll need PHP.
+
+### Configuration
+Copy config.json.example to config.json, edit it and add add your own token from https://api.slack.com/custom-integrations/legacy-tokens
+
+## License
+SLAIL is an open-source software licensed under the MIT license
---
+++ app/Commands/TailCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+use PhpSlackBot\Bot;
+use App\TailCatchallCommand;
+
+
+class TailCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'tail {--channel= : Tail the specific channel}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Tail a slack channel';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $config = json_decode(file_get_contents("config.json"));
+        if(!$config) {
+            die("Please configure me.");
+        }
+
+        if(!$config->token) {
+            die("Please token me.");
+        }
+
+        $this->info('Slinking into slack...');
+
+        $logger = new \Zend\Log\Logger();
+        $writer = new \Zend\Log\Writer\Stream("php://output");
+
+        $filter = new \Zend\Log\Filter\Priority(\Zend\Log\Logger::CRIT);
+        $writer->addFilter($filter);
+        $logger->addWriter($writer);
+
+        $bot = new Bot();
+        $bot->setToken($config->token);
+        $bot->initLogger($logger);
+        $bot->loadCommand(new TailCatchallCommand());
+        $bot->loadInternalCommands(); // This loads example commands
+        $bot->run();
+    }
+
+    /**
+     * Define the command's schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    public function schedule(Schedule $schedule)
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+}
