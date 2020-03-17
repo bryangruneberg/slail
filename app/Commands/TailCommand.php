da85b1fc58980fd28301dfc482b4916f9abefc9d
Bryan Gruneberg <hello@bryangruneberg.com>
2020-03-17 23:02:05 +0100

    Filters and messages and stuff

--- app/Commands/TailCommand.php
+++ app/Commands/TailCommand.php
@@ -4,9 +4,61 @@ namespace App\Commands;
 
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
+use Illuminate\Support\Facades\Cache;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class TailCommand extends Command
 {
+    protected $regexMessageWhitelistFilters = [];
+
+    public function addMessageWhitelistRegexMatch($regex)
+    {
+        $this->regexMessageWhitelistFilters[] = $regex;
+    }
+
+    public function messageMatchesWhitelist($message)
+    {
+      $return = FALSE;
+
+      foreach($this->regexMessageWhitelistFilters as $regex) {
+        if($this->isVerbose()) {
+            $this->info("Checking message regex: " . $regex);
+        }
+
+        if(preg_match($regex, $message)) {
+            $return = TRUE;
+            break;
+        } 
+      }
+
+      return $return;
+    }
+
+    protected $regexSendersWhitelistFilters = [];
+
+    public function addSendersWhitelistRegexMatch($regex)
+    {
+        $this->regexSendersWhitelistFilters[] = $regex;
+    }
+
+    public function senderMatchesWhitelist($sender)
+    {
+      $return = FALSE;
+    
+      foreach($this->regexSendersWhitelistFilters as $regex) {
+        if($this->isVerbose()) {
+            $this->info("Checking sender '{$sender}' against regex: " . $regex);
+        }
+
+        if(preg_match($regex, $sender)) {
+            $return = TRUE;
+            break;
+        } 
+      }
+
+      return $return;
+    }
+
     /**
      * The signature of the command.
      *
@@ -28,15 +80,134 @@ class TailCommand extends Command
      */
     public function handle()
     {
+        $this->addMessageWhitelistRegexMatch('/.*/');
+        $this->addSendersWhitelistRegexMatch('/Blaize Kaye/');
+        $this->addSendersWhitelistRegexMatch('/Bryan Gruneberg/');
+
         $slackTail = new \App\SlackTail(env('AMAZEE_SLACK_TOKEN'));
-        $slackTail->addRegexFilter('/.*/');
+        
 
         $output = $this->output;
-        $slackTail->tail(function($match) use ($output) {
-            $data = $match['data'];
-            print_r($data);
-            $channel = $data['conversation']['channel']['name_normalized'] ?? '__none__';
-            $output->writeln('<fg=red>' . $channel . ':</> ' . $data['text']);
+        $slackTail->tail(function($data) use ($output) {
+            if($data['subtype'] == "message_deleted") {
+                return;
+            }
+
+            if(empty($data['text']) && isset($data['message_data']['text'])) {
+                $data['text'] = $data['message_data']['text'];    
+            }
+
+            $sender = $this->findUserName($data) ?? '__user__';
+            $outline = $this->getFormattedOutput($data);
+            if($this->messageMatchesWhitelist($outline) && $this->senderMatchesWhitelist($sender)) {
+                $output->writeln($outline);
+            }
+            
+            if(isset($data['message_data']['attachment_simplifications'])) {
+                foreach($data['message_data']['attachment_simplifications'] as $attachment) {
+                    $adata = [
+                        'user_info' => $data['message_data']['user_info'],
+                        'conversation' => $data['conversation']
+                    ];
+
+                    if(isset($data['message'])) {
+                        $adata['message'] = $data['message'];
+                    }
+
+                    $adata['text'] = " {+++} " . $attachment;
+                    $sender = $this->findUserName($adata) ?? '__user__';
+                    $outline = $this->getFormattedOutput($adata);
+                    if($this->messageMatchesWhitelist($outline) && $this->senderMatchesWhitelist($sender)) {
+                        $output->writeln($outline);
+                    }
+                }
+            }
+        }, 
+        function($reactionData) use ($output) {
+            $sender = $this->findUserName($reactionData) ?? '__user__';
+            $outline = $this->getFormattedOutput($reactionData);
+            
+            if($this->messageMatchesWhitelist($outline) && $this->senderMatchesWhitelist($sender)) {
+                $output->writeln($outline);            
+            }
         });
+    }
+
+    public function getFormattedOutput($data) {
+        $user = $this->findUserName($data) ?? '__user__';
+        $channel = $this->findChannelName($data) ?? '__channel__';
+
+        $prefix = "<fg={$this->getColorFor($channel)}>#" . $channel . '</>' .
+            ': ' . 
+            "<fg={$this->getColorFor($user)}>@" . $user . '</>';
+    
+
+        if(isset($data['thread_ts'])) {
+            $prefix = $prefix . " ({$data['thread_ts']}) ";
+        }
+
+        if(isset($data['type']) && $data['type'] == "reaction_added") {
+            return  $prefix . '] {{' . $data['reaction_item']['text'] .'}} ==> <options=underscore,reverse> ++' . $data['reaction'] . '</>';
+        } else {
+            return  $prefix . '] ' . $data['text'];
+        }
+    }
+
+    public function findChannelName($data)
+    {
+        if(isset($data['conversation']['channel']['name_normalized'])) {
+            return $data['conversation']['channel']['name_normalized'];
+        }
+
+        if(isset($data['conversation']['with_user']['user']['name'])) {
+            return $data['conversation']['with_user']['user']['name'];
+        }
+    }
+
+    public function findUserName($data)
+    {
+        if(isset($data['user_info']['user']['real_name'])) {
+            return $data['user_info']['user']['real_name'];
+        }
+
+        if(isset($data['message_data']['user_info']['user']['real_name'])) {
+            return $data['message_data']['user_info']['user']['real_name'];
+        }
+    }
+
+    public function getColorFor($for) {
+        $colors = [
+            "red", "green", "yellow", "blue", "magenta", "cyan", "white"
+        ];
+
+        $lastColorCacheKey = "color-for-last-color-index";
+        $lastColorIndex = Cache::get($lastColorCacheKey, 0);
+
+        $cacheKey = "color-for-" . $for;
+        if(Cache::has($cacheKey)) {
+            $color = Cache::get($cacheKey);
+        } else {
+            if($lastColorIndex >= count($colors)) { 
+                $lastColorIndex = 0;
+            }
+
+            $color = $colors[$lastColorIndex];
+            $lastColorIndex++;
+            Cache::put($cacheKey, $color, now()->addMinutes(2));
+            Cache::put($lastColorCacheKey, $lastColorIndex);
+        }
+
+        return $color;
+    }
+
+    public function isVerbose()
+    {
+        $verbosityLevel = $this->getOutput()->getVerbosity();
+
+        if($verbosityLevel >= OutputInterface::VERBOSITY_VERBOSE){
+            return TRUE;
+        }      
+        
+        return FALSE;
     }
 }
--- app/SlackTail.php
+++ app/SlackTail.php
@@ -10,39 +10,142 @@ use Illuminate\Support\Facades\Cache;
 class SlackTail
 {
     private $token;
-    protected $regexFilters = [];
+    protected $slack;
 
     protected $convoCache = [];
 
     public function __construct($token)
     {
         $this->token = $token;
+        $this->slack = new SlackWrapi($this->token);
     }
 
-    public function addRegexFilter($regex)
+    public function enrichChannelData(&$data)
     {
-        $this->regexFilters[] = $regex;
+      $slack = $this->slack;
+      if(! empty($data['channel'])) {
+          $cachKey = 'channel-' . $data['channel'];
+          if(Cache::has($cachKey)) {
+            $data['conversation'] = Cache::get($cachKey);;
+          } else {
+            $convo = $slack->conversations->info(array("channel" => $data['channel']));
+            if( isset($convo['ok'])) {
+              $isIm = $convo['channel']['is_im'] ?? FALSE;
+              if($isIm) {
+                $withUser = $this->getUserData($convo['channel']['user']);
+                $convo['with_user'] = $withUser;
+              }
+
+              $data['conversation'] = $convo;
+            }
+
+            Cache::put($cachKey, $convo, now()->addMinutes(2));
+          }
+      }
+
+      $data['text'] = $this->prettifyChannelIds($data['text']);
+      
+    }
+
+    public function getUserData($userId)
+    {
+      $slack = $this->slack;
+      $cachKey = 'user-' . $userId;
+      $data = null;
+      if(Cache::has($cachKey)) {
+        $data = Cache::get($cachKey);;
+      } else {
+        $user = $slack->users->info(array("user" => $userId));
+        if( isset($user['ok'])) {
+          $data = $user;
+          Cache::put($cachKey, $data, now()->addMinutes(2));
+        }   
+      }
+      return $data;
+    }
+
+    public function enrichUserData(&$data)
+    { 
+      if(! empty($data['user'])) {
+          $data['user_info'] = $this->getUserData($data['user']);
+      }
+      
+      $data['text'] = $this->prettifyUserIds($data['text']);
+      
+    }
+
+    public function prettifyChannelIds($text)
+    {
+      if(preg_match_all('/<#(C\w+\|[\w_-]+)>/', $text, $matches)) {
+        foreach($matches[0] as $index => $match) {
+          if(preg_match("/(.*)\|([\w_-]+)/", $match, $parts)) {
+            $text = str_replace($matches[0][$index], "#" . $parts[2], $text);
+          }
+        }
+      }
+
+      return $text;
+    }
+
+    public function prettifyUserIds($text)
+    {
+      if(preg_match_all('/<@(U\w+)>/', $text, $matches)) {
+        foreach($matches[1] as $index => $match) {
+          $userData = $this->getUserData($match);
+          if(isset($userData['user']['real_name'])) {
+            $text = str_replace($matches[0][$index], $userData['user']['real_name'], $text);
+          }
+        }
+      }
+
+      return $text;
+    }
+
+    public function enrichAttachments(&$data) 
+    {
+      $attachment_simplifications = [];
+      if(isset($data['attachments'])) {
+        foreach($data['attachments'] as $index => $attachment) {
+          if(isset($data['attachments'][$index]['text'])) {
+            $text = $data['attachments'][$index]['text'];
+            $text = $this->prettifyUserIds($text);
+            $text = $this->prettifyChannelIds($text);
+            $attachment_simplifications[] = $text;
+          }
+        }
+      }
+
+      if(count($attachment_simplifications) > 0) {
+        $data['attachment_simplifications'] = $attachment_simplifications;
+      }
     }
 
     public function enrichSlackData(&$data) 
     {
-        $slack = new SlackWrapi($this->token);
-        if(! empty($data['channel'])) {
-            $cachKey = 'channel-' . $data['channel'];
-            if(Cache::has($cachKey)) {
-              $data['conversation'] = Cache::get($cachKey);;
-            } else {
-              $convo = $slack->conversations->info(array("channel" => $data['channel']));
-              if( isset($convo['ok'])) {
-                $data['conversation'] = $convo;
-              }
-
-              Cache::put($cachKey, $convo, now()->addMinutes(2));
-            }
-        }
+        $this->enrichChannelData($data);
+        $this->enrichUserData($data);
+        $this->enrichAttachments($data);
     }
 
-    public function tail(callable $matchFunction, callable $nomatchFunction = null)
+    public function getItemByChannelTS($channelId, $TS)
+    {
+      $slack = $this->slack;
+      $cachKey = 'items-' . $channelId . '-' . $TS;
+      $data = null;
+      if(Cache::has($cachKey)) {
+        $data = Cache::get($cachKey);;
+      } else {
+        $item = $slack->conversations->history(array("channel" => $channelId, "latest" => $TS, "limit" => 1, "inclusive" => true));
+        if($item['ok'] == 1 && isset($item['messages'][0])) {
+          $data = $item['messages'][0];
+          Cache::put($cachKey, $data, now()->addMinutes(2));
+        }
+      }
+
+      return $data;
+    }
+
+    public function tail(callable $messageFunction, callable $reactionAdded = null)
     {
 
         $loop = ReactEventLoopFactory::create();
@@ -51,18 +154,33 @@ class SlackTail
         $client->setToken($this->token);
         $client->connect();
 
-        $client->on('message', function ($data) use ($client, $matchFunction, $nomatchFunction) {
+        $client->on('message', function ($data) use ($client, $messageFunction) {
             $this->enrichSlackData($data);
-
-            foreach($this->regexFilters as $regex) {
-              if(preg_match($regex, $data['text'], $match)) {
-                $matchFunction(['match' => $match,'data' => $data]);
-              } else {
-                if($nomatchFunction) {
-                  $nomatchFunction(['match' => null, 'data' => $data]);
-                }
-              }
+            if(isset($data['message']) && is_array($data['message'])) {
+              $message_data = $data['message'];
+              $this->enrichSlackData($message_data);
+              $data['message_data'] = $message_data;
             }
+
+            $messageFunction($data);        
+        });
+
+        $client->on('reaction_added', function ($data) use ($reactionAdded) {
+          if($reactionAdded) {
+          
+            if(isset($data['user'])) {
+              $this->enrichUserData($data);
+            }
+
+            if(isset($data['item']['channel'])) {
+              $data['channel'] = $data['item']['channel'];
+              $this->enrichChannelData($data);
+            }
+
+            $data['reaction_item'] = $this->getItemByChannelTS($data['item']['channel'], $data['item']['ts']);
+
+            $reactionAdded($data);
+          }
         });
 
         $loop->run();
